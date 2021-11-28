b76cbbf4a69be0224b14ad7e69dc62904b344dfd
shinjini25 <shinjinibh2503@gmail.com>
2021-11-28 22:44:24 +0530

    changed files (28/11)

---
+++ backend/fetchDarwinSchedule.php
@@ -0,0 +1,54 @@
+<?php
+
+        
+
+        $servername = "localhost";
+
+        $username = "riidlorg_d2021";
+
+        $password = "darwin@riidl520";
+
+        $dbname = "riidlorg_darwin2021";
+
+
+
+        // Create connection
+
+        $conn = new mysqli($servername, $username, $password, $dbname);
+
+        // Check connection
+
+        if ($conn->connect_error) {
+
+            die("Connection failed: " . $conn->connect_error);
+
+        }
+
+
+
+        //start code
+
+        $output = "";
+
+        $sql= "SELECT * FROM `schedule` WHERE `organized_by`= 'darwin'";
+
+        $result = mysqli_query($conn,$sql);
+
+        $output = array();
+
+        while($row = mysqli_fetch_array($result)){
+
+            array_push($output,$row);
+
+        }
+
+    
+
+        echo json_encode($output);
+
+                
+
+        $conn->close();
+
+?>
+

1e1adafa22014a4d81dd3ade570c07b133b95163
shinjini25 <shinjinibh2503@gmail.com>
2021-12-02 10:36:27 +0530

    help desk- resolved

---
+++ backend/fetchContactsRC.php
@@ -0,0 +1,57 @@
+<?php
+
+
+  $attendee_rc = $_POST['attendee_rc']; 
+  
+        $servername = "localhost";
+
+        $username = "riidlorg_d2021";
+
+        $password = "darwin@riidl520";
+
+        $dbname = "riidlorg_darwin2021";
+
+
+
+        // Create connection
+
+        $conn = new mysqli($servername, $username, $password, $dbname);
+
+        // Check connection
+
+        if ($conn->connect_error) {
+
+            die("Connection failed: " . $conn->connect_error);
+
+        }
+
+
+
+        //start code
+
+        $output = "";
+
+           $sql= "SELECT * FROM `rc_credentials` WHERE `name`= '$attendee_rc'";
+        
+    
+
+        $result = mysqli_query($conn,$sql);
+
+        $output = array();
+
+        while($row = mysqli_fetch_array($result)){
+
+            array_push($output,$row);
+
+        }
+
+    
+
+        echo json_encode($output);
+
+                
+
+        $conn->close();
+
+?>
+
